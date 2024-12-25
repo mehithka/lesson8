5ddfa61e0189ca08e3b0165994b2528033d48757
mehithka <mehith.karacasia@gmail.com>
2024-12-25 15:32:45 +0500

    a2

---
+++ a2.py
@@ -0,0 +1,9 @@
+string1 = str(input("enter your name or any word : "))
+
+string2 = " "
+
+for i in string1 :
+    string2 = i + string2 
+
+print("the original string: ", string1)
+print("\n The new string : ", string2)
